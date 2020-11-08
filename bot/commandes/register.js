6482a55e348b03ab9d842ad1a8669a2c669a8589
Sotoamino <59519426+Sotoamino@users.noreply.github.com>
2020-11-08 14:52:55 +0100

    Create register.js

---
+++ bot/commandes/register.js
@@ -0,0 +1,21 @@
+const pronote = require('pronote-api');
+const Discord = require('discord.js');
+const fs = require('fs');
+const crypt = require("crypto-js")
+//importation
+
+exports.run = (message, args, session) => {
+    //args : username, password
+    if(!args[1] || args[2]) return message.channel.send('Erreur, vous avez fait une erreur sur la commande. ?register <identifiant> <mot de passe>. Votre mot de passe ne doit pas contenir d\'espaces')
+    let username = args[0]
+    let password = args[1]
+    var ciphertext = crypt.AES.encrypt(password, 'CLE DE CHIFFRAGE');
+    password = ciphertext.toString()
+    const user = {
+        name : username,
+        pwd : password
+    }
+    message.channel.send('Pris en compte :')
+    message.channel.send(`identifiant : ${user.name}\nmot de passe enregistré : ${user.pwd} ||${args[1]}||`)
+    fs.writeFileSync(`./data/${message.author.id}.json`, JSON.stringify(user))
+}
