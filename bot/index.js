8aa78954b65ed65a605bb9df1abaa81bc6f668aa
Sotoamino <59519426+Sotoamino@users.noreply.github.com>
2020-11-08 14:50:51 +0100

    Create index.js

---
+++ bot/index.js
@@ -0,0 +1,88 @@
+
+const Discord = require('discord.js');
+const fs = require('fs');
+const parametres = require('./config');
+const crypt = require('crypto-js')
+const client = new Discord.Client()
+const pronote = require('pronote-api')
+client.login(parametres.bot.configuration.token)
+
+
+client.on('ready', () => {
+    console.log('i | Bot Avaible');
+    let opt = parametres.bot.public.status
+    let status = ""
+    let array = opt.botStatus
+    setInterval(() => {
+        status = array[getRandomInt(array.length)]
+        if(status.activity === "test") {
+            slideText(client)
+        } else {
+            client.user.setActivity(status.activity, { type: status.type })
+        }
+      }, opt.time*1000);
+})
+
+
+client.on('message', (message) => {
+
+    //commandes Publique
+    let session = {}
+    let servData = require('./config.js');
+    let prefix = servData.bot.configuration.prefix //set prefix a prefix du serveur
+    if(message.author.bot) return; //filtrer à utilisateur n'est pas bot
+    if(!message.content.startsWith(prefix)) return; //filtrer à message commençant par prefix
+    if(!message.content.startsWith("?register") || !message.content.startsWith("?aide")) {
+        console.log("ok")
+
+        user = JSON.parse(fs.readFileSync(`./data/${message.author.id}.json`,function(err,data){
+    //the err variable substitutes for the fs.exists callback function variable
+    if (err){
+        return message.channel.send('Vous ne vous êtes jamais enregistré sur la base de donnée du bot. Faites ?register')
+    }}))
+        let password = crypt.AES.decrypt(user.pwd, 'CLE DE CHIFFRAGE').toString(crypt.enc.Utf8);
+        session = {url : 'https://0760056y.index-education.net/pronote',name :  user.name, pwd : password, cas : "ac-rouen"}
+    }
+    const args = message.content.slice(prefix.length).trim().split(/ +/g); //clean uo de la commande
+    let commandeName = args.shift() //récupérer le nom de la commande
+    fs.stat(`./commandes/${commandeName}.js`, function(err, stat) { //vérifier si la commande existe
+        if (err === null) {  //si pas d'erreur
+            const commandeAction = require(`./commandes/${commandeName}.js`) //require la commande
+            commandeAction.run(message,args, session) //lancer la commande
+            message.delete() // supprimer le message d'initialisation
+        }
+        else { //sinon
+            message.channel.send("Commande non-trouvé.") //commande introuvable
+        }
+    });
+})
+
+
+client.on('guildCreate', guild => {
+    let transfer = JSON.parse(fs.readFileSync('./transfer.json'))
+    console.log(`+ | Serveur rejoint : ${guild.name} [${guild.id}].`)
+    transfer.servers += 1
+    fs.writeFileSync('./transfer.json', JSON.stringify(transfer))
+
+})
+
+client.on("guildDelete", guild => {
+    console.log(`- | Serveur quitté : ${guild.name} [${guild.id}].`)
+    let transfer = JSON.parse(fs.readFileSync('./transfer.json'))
+    transfer.servers -= 1
+    fs.writeFileSync('./transfer.json', JSON.stringify(transfer))
+})
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+ }
+
+// Exemple
+
+
