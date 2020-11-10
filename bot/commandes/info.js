c1bfbe1867b32f576e2c5cd9dfc105bd0ca74b56
Sotoamino <59519426+Sotoamino@users.noreply.github.com>
2020-11-10 09:39:15 +0100

    Mise à jour des commandes

--- bot/commandes/info.js
+++ bot/commandes/info.js
@@ -5,25 +5,37 @@ const Discord = require('discord.js');
 exports.run = (message, args, user) => {
 async function main()
 {
+    console.log(`Requête API : ${message.author.username} sur PronoteAPI via utilisateur ${user.name}`)
     session = await pronote.login(user.url, user.name, user.pwd, user.cas);
+	console.log('Requête API : Connexion ...')
     const embed = new Discord.MessageEmbed()
+        .setColor("#138266")
         .setTitle(`${session.type.value} - ${session.user.name}`)
         .setDescription(`${session.type.value} de ${session.user.studentClass.name} au lycée ${session.user.establishment.name}`)
         .setFooter(session.user.id+" - "+session.user.establishment.id)
         .setThumbnail(session.user.avatar)
-    message.channel.send(embed)
+    message.channel.send(embed).then(() => {
+        console.log('Requête API: Accepté')
+    })
 }
 
 main().catch(err => {
-    if (err.code === pronote.errors.WRONG_CREDENTIALS.code) {
-        return message.channel.send('Mauvais identifiants');    
-    } if(err.response.status === 503) {
-        return message.channel.send("Erreur, l'accès à l'ENT est actuellemement indisponible.")
-
+        if(err) {
+        console.error('Requête API : ERROR !')
     }
-    
+    if (err.code === pronote.errors.WRONG_CREDENTIALS.code) {
+        console.error(`${message.author.username} - Mauvais identifiants`)
+        return message.channel.send('Mauvais identifiants');    
+    } if(err.code === "ETIMEDOUT") {
+        console.error(`${message.author.username} - Problème de connexion`)
+        return message.channel.send('Erreur de connexion, veuillez refaire la commande.')
+    }if(err.response) {
+        if(err.response.status === 503){
+            console.error(`${message.author.username} - Pas d'accès ENT`)
+            return message.channel.send("Erreur, l'accès à l'ENT est actuellemement indisponible.")
+        }
+    }
     else {
-        console.error(err);
     }
 });
 }
